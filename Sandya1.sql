8854029a8a9a2afe2b3874ad5e8683e3e84fe384
Sandya <ssomasoundaram@gmail.com>
2017-11-30 19:39:05 +0100

    insertion données

---
+++ Sandya1.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO listecourse VALUES(1, 34,20.24);
+--Bon ordre pour insérer les valeurs (pour supprimer table ordre inverse)--
+INSERT INTO COMMANDE values(1,4,1,2);
+INSERT INTO PRODUIT VALUES (1,'jus',4,0.5);
+INSERT INTO EST_COMPOSE_DE VALUES (4,1,2);
+INSERT INTO COMPTE VALUES(1,1,1,'12/08/2016');
+INSERT INTO ENFANT VALUES (1,'SOMA','Surya','14/02/2000','dodo@gmail.com',0169215413,'moins de 6 ans',10.5,4.5,1,1);
+INSERT INTO UTILISATEUR VALUES(1, 'sandya','triples',1);
+
